fee8aa4c372a4249c167b2e05425cf433fad8173
Aakash Mangla <akkibaby90@yahoo.com>
2020-06-05 20:49:39 +0530

    Created the project and did some basic work

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from main import models
+
+# Create your views here.
+
+def index(request):
+    latest_articles = models.Article.objects.all().order_by('-createdAt')[:10]
+
+    context = {
+        "latest_articles": latest_articles
+    }
+
+    return render(request, 'main/index.html', context)

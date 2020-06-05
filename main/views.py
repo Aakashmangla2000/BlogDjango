981bd312bd856a9e4b93dd26a5694641c2ca2c70
Aakash Mangla <akkibaby90@yahoo.com>
2020-06-06 00:46:58 +0530

    Completed project and added materialize library

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from main import models
 
@@ -12,3 +12,43 @@ def index(request):
     }
 
     return render(request, 'main/index.html', context)
+
+def article(request, pk):
+    # article = models.Article.objects.get(pk = pk)
+    article = get_object_or_404(models.Article, pk = pk)
+
+    context = {
+        "article": article
+    }
+
+    return render(request, 'main/article.html', context)
+
+def author(request, pk):
+
+    author = get_object_or_404(models.Author, pk = pk)
+
+    context = {
+        "author": author
+    }
+
+    return render(request, 'main/author.html', context)
+
+def create_article(request):
+    
+    author = models.Author.objects.all()
+    context = {
+        "authors": author,
+    }
+
+    if(request.method == "POST"):
+        article_data = {
+        "title": request.POST['title'],
+        "content": request.POST["content"]
+        }
+        article = models.Article.objects.create(**article_data)
+        author = models.Author.objects.filter(pk = request.POST['author'])
+        article.authors.set(author)
+        context["success"] = True
+
+    
+    return render(request, 'main/create_article.html', context)
